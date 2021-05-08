a928800511cbabf1d5bff044d5e1df575d527ee2
Richard Grand'Maison <rich.grandmaison@gmail.com>
2021-05-08 10:41:33 -0400

    Vishal modifications

--- app.js
+++ app.js
@@ -1,48 +1,43 @@
+// THIS IS IN LONDON
+
 // import required modules
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
-const app = express();
-app.use(cors());
+const $express = express();
+$express.use(cors());
 const port = 3000;
 
 // Makes it easier to read JSON
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+$express.use(express.json())
+$express.use(express.urlencoded({extended: true}))
 
-const connection = mysql.createConnection({
+const databaseConnection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'rootroot',
   database: 'expressdb',
-  port: 3306
 })
 
 // Get All Students
-app.get('/students', (req, res) => {
-
-  console.log("Fetching all students ...")
-
-  const sql = 'SELECT * FROM student'
-  connection.query(sql, (err, rows) => {
+$express.get('/students', (req, response) => {
+  const sql = 'SELECT * FROM student';
+  databaseConnection.query(sql, (err, rows) => {
     if (err) throw err;
-    console.log('rows', rows);
-    res.json(rows);
+    response.json(rows);
   });
-
-  console.log('This is after going to the database!');
 })
 
 // Get Student By Id
 // students/7
-app.get('/students/:id', (req, res) => {
+$express.get('/students/:id', (req, res) => {
 
   console.log('params:', req.params);
 
   const id = req.params.id;
   console.log(`Fetching student with id ${id}...`);
 
-  connection.query(
+  databaseConnection.query(
     `SELECT * FROM student WHERE id = ${id}`, 
     (err, rows) => {
       if (err) throw err;
@@ -51,10 +46,10 @@ app.get('/students/:id', (req, res) => {
 })
 
 // Login
-app.post('/login', (req, res) => {
+$express.post('/login', (req, res) => {
   console.log('trying to login!')
 
-  connection.query(
+  databaseConnection.query(
     'do the sql!',
     (err, rows) => {
       if (err) throw err;
@@ -69,13 +64,15 @@ app.post('/login', (req, res) => {
 })
 
 // Create a Student
-app.post('/students', (req, res) => {
+$express.post('/students', (request, res) => {
   console.log(`Creating student ... `)
 
-  console.log('body', req.body)
+  console.log(`request`, request);
+
+  console.log('body', request.body)
  
-  connection.query(
-    `INSERT INTO student (name, sex) VALUES ('${req.body.name}','${req.body.sex}')`,
+  databaseConnection.query(
+    `INSERT INTO student (name, sex) VALUES ('${request.body.name}','${request.body.sex}')`,
     (err, rows) => {
       if (err) throw err;
       res.send(rows);
@@ -83,12 +80,12 @@ app.post('/students', (req, res) => {
 });
 
 // Update a Student
-app.put('/students/:id', (req, res) => {
+$express.put('/students/:id', (req, res) => {
   console.log('params:', req.params);
   const id = req.params.id;
   console.log("body:", req.body)
 
-  connection.query(
+  databaseConnection.query(
       `UPDATE student SET name = '${req.body.name}', sex = '${req.body.sex}' ` +
       `WHERE id = ${id}`,
       (err, rows) => {
@@ -98,10 +95,10 @@ app.put('/students/:id', (req, res) => {
 });
 
 // Delete a student
-app.delete('/students/:id', (req, res) => {
+$express.delete('/students/:id', (req, res) => {
   console.log(`Delete student id [${req.params.id}]... `)
 
-  connection.query(
+  databaseConnection.query(
     `DELETE FROM student WHERE id = ${req.params.id}`,
     (err, rows) => {
       if (err) throw err;
@@ -111,6 +108,6 @@ app.delete('/students/:id', (req, res) => {
 
 // Listen
 // localhost => 127.0.0.1
-app.listen(port, () => {
+$express.listen(port, () => {
   console.log(`Student app listening at http://localhost:${port}`)
 })
